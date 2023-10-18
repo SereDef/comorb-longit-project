c127573019e6cfa725d390e3eddafba12cd5daed
SereDef <sere.def@gmail.com>
2023-10-18 11:41:37 +0200

    adapt for render smaller fonts

--- src/definitions/layout_styles.py
+++ src/definitions/layout_styles.py
@@ -1,25 +1,25 @@
 
-TITLE = {'font-size': '36px', 'text-align': 'center', 'font-weight': 'bold'}
-NAVBAR = {'font-size': '18px'}
+TITLE = {'font-size': '32px', 'text-align': 'center', 'font-weight': 'bold'}
+NAVBAR = {'font-size': '16px'}
 
-TEXT = {'font-size': '12px'}
-SUB_TITLE1 = {'font-size': '14px', 'text-align': 'left'}
-SUB_TITLE2 = {'font-size': '14px', 'text-align': 'center'}
+TEXT = {'font-size': '14px'}
+SUB_TITLE1 = {'font-size': '16px', 'text-align': 'left'}
+SUB_TITLE2 = {'font-size': '16px', 'text-align': 'center'}
 
-OVERVIEW_WIDTH = 1600
+OVERVIEW_WIDTH = 1500
 OVERVIEW_LABELS = {'size': OVERVIEW_WIDTH/100-1}
 
-CLPM_WIDTH = 800
+CLPM_WIDTH = 850
 CLPM_NODE_LABEL = 12
 CLPM_NODE_SIZE = 18
 CLPM_EDGE_LABEL = 10
 
 CLNM_WIDTH = 420
-CLNM_V_POS = '40vh'
+CLNM_V_POS = '50vh'
 CLNM_NODE_LABEL = 12
 CLNM_NODE_SIZE = 22
 
-CSNM_WIDTH = 620
-CSNM_V_POS = '50vh'
+CSNM_WIDTH = 610
+CSNM_V_POS = '55vh'
 CSNM_NODE_LABEL = 14
 CSNM_NODE_SIZE = 23
